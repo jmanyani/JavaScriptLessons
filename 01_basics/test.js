634a4c655e31b080ec06386e1c04aa0eedcdd7c1
Jatin Manyani <jmanyani@depaul.edu>
2024-12-01 00:47:28 +0000

    js continued

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Soon I will have amazing resume ready projects with interview Cracking Skills")
+// console.log("Soon I will have amazing resume ready projects with interview Cracking Skills")
+
+const myFunction=function(){
+console.log("Trying Js Functions for the first time")   
+}
+myFunction();
+console.log(myFunction)
+
+const my_symbol=Symbol('03221998');
+console.log(my_symbol);
+console.log(typeof(my_symbol));
