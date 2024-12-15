6e8b0a825da7d3af3cca250d75ba167a40a6d0af
Jatin Manyani <jmanyani@depaul.edu>
2024-12-15 00:43:54 +0000

    js_continued

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,21 @@
-(()=>console.log("Hello World !"))();
-(()=>console.log("Hello Jatin Bro !"))();
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+let bill=shoppingCart.reduce((acc,currentValue)=>acc+currentValue.price,0)
+console.log(bill)
